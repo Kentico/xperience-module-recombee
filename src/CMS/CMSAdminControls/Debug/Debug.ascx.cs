74cc46fefc7bcd9a60d110114045a34610fe45bc
Stanislav Sojka <StanislavS@kentico.com>
2021-01-22 14:23:38 +0100

    KX-442 - Add accidentally excluded controls

---
+++ src/CMS/CMSAdminControls/Debug/Debug.ascx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+using CMS.Base;
+using CMS.Base.Web.UI;
+using CMS.UIControls;
+
+
+public partial class CMSAdminControls_Debug_Debug : CMSUserControl
+{
+    private readonly List<LogControl> logControls = new List<LogControl>();
+
+
+    protected override void OnInit(EventArgs e)
+    {
+        base.OnInit(e);
+
+        ScriptHelper.RegisterModule(this, "CMS/DebugControl", pnlDebugContainer.ClientID);
+    }
+
+
+    protected override void Render(HtmlTextWriter writer)
+    {
+        // Do not render if nothing is displayed
+        foreach (var logControl in logControls)
+        {
+            if (logControl.Visible)
+            {
+                base.Render(writer);
+
+                DebugContext.DebugPresentInResponse = true;
+
+                break;
+            }
+        }
+    }
+}
